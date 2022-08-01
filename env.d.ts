2c4f6f397b589865d392369f17749daa46bc64e4
thornboo <zhaozhuang1210@gmail.com>
2022-08-01 14:44:31 +0800

    :wrench: chore(config): fix project code format and add commit specification

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,7 @@
 // vue文件类型声明
 declare module '*.vue' {
-      import { DefineComponent } from 'vue'
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
-      const component: DefineComponent<{}, {}, any>
-      export default component
-    }
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+  import { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
