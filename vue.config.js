aae008f756de8ab73a7df1e89318ee9c129c6294
thornboo <zhaozhuang1210@gmail.com>
2022-08-04 11:17:13 +0800

    :wrench: chore(config): fix uni-app global configuration file

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
+/**
+ * vue.config.js 是一个可选的配置文件，如果项目的根目录中存在这个文件，那么它会被自动加载
+ * 一般用于配置 webpack 等编译选项
+ */
+const path = require('path')
+
 module.exports = {
+  // 通过 Babel 显式转译一个依赖
   transpileDependencies: ['uview-ui'],
 }
