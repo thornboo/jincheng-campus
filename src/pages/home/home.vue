20c38d90624e56c7f886aaa2e59ffd6e326f64aa
thornboo <zhaozhuang1210@gmail.com>
2022-08-11 08:48:36 +0800

    :bug: fix(components): 修复通知栏的圆角问题

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,30 +1,27 @@
 <template>
-  <view class="swiper-container">
-    <swiper indicator-dots="true" autoplay="true" circular="true">
+  <view class="swiper-container base-container">
+    <swiper autoplay="true" circular="true">
       <swiper-item>
-        <view class="A text-swiper swiper-item-box">图片A</view>
+        <view class="swiper-item-box"></view>
       </swiper-item>
       <swiper-item>
-        <view class="B text-swiper swiper-item-box">图片B</view>
-      </swiper-item>
-      <swiper-item>
-        <view class="C text-swiper swiper-item-box">图片C</view>
+        <view class="swiper-item-box"></view>
       </swiper-item>
     </swiper>
   </view>
 
-  <view class="notice-container">
+  <view class="notice-container base-container">
     <van-notice-bar
-      custom-class="notice-bar-box"
       scrollable
       background="#ecf9ff"
       color="#1989fa"
       left-icon="info-o"
+      custom-class="van-notice-bar"
       text="程序还在开发中，敬请期待~"
     />
   </view>
 
-  <view class="grid-container">
+  <view class="grid-container base-container">
     <van-grid square column-num="4" gutter="10" clickable="true">
       <van-grid-item icon-color="#fca626" icon="photo-o" text="失物招领" :badge="value[0]" />
       <van-grid-item icon-color="#fca626" icon="photo-o" text="闲置交易" :badge="value[1]" />
@@ -39,61 +36,68 @@
 </template>
 
 <script setup>
-const value = [10, 20, 30, 40, 50, 60]
+const value = [14, 25, 32, 48, 55, 61]
 </script>
 
 <style lang="scss">
+/**
+  公共类
+ */
 page {
   background-color: #8a8888;
+  // 通知栏-样式变量
   --notice-bar-height: 30px;
 }
 
+.base-container {
+  margin-left: 10px;
+  margin-right: 10px;
+  margin-bottom: 10px;
+}
+
+/**
+  轮播图
+ */
 .swiper-container {
+  height: 200px;
   margin-top: 10px;
-  margin-bottom: 10px;
 }
 
+// 轮播图item
 .swiper-item-box {
-  margin-left: 10px;
-  margin-right: 10px;
+
+
+  height: 200px;
   border-radius: 10px;
 }
 
-.A {
-  background-color: $uni-color-primary;
-  height: 120px;
-}
-
-.B {
-  background-color: $uni-color-success;
-  height: 120px;
-}
-
-.C {
-  background-color: $uni-color-warning;
-  height: 120px;
-}
-
-// 轮播图中的文字（暂时性）
-.text-swiper {
-  height: 150px;
-  line-height: 150px;
-  text-align: center;
-}
-
+/**
+  通知栏
+ */
 .notice-container {
-  margin-left: 10px;
-  margin-right: 10px;
-  border-radius: 10px;
-  margin-bottom: 10px;
 }
 
-.grid-container {
-  margin-bottom: 10px;
-}
-
-.notice-bar-box {
+// 通知栏-外部样式类
+.van-notice-bar {
   border-radius: 10px !important;
-  background-color: #9c26b0 !important;
+}
+
+/**
+  宫格
+ */
+.grid-container {
+}
+
+/**
+  演示样式（可替换删除）
+ */
+.A-view {
+  background-color: $uni-color-primary;
+  height: 200px;
+}
+
+.B-view {
+  background-color: $uni-color-success;
+  height: 200px;
 }
 </style>
