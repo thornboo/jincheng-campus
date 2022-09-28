fbb6142128a2477ae7f62a9b1c50d917c10b28a9
thornboo <zhaozhuang1210@gmail.com>
2022-09-28 21:11:39 +0800

    :bug: fix(components): fixed a mismatch between highlighted and currently selected ICONS in the Tabbar component
    But there is no way to switch pages through Tabbar

--- components/TabBar/tabBar.vue
+++ components/TabBar/tabBar.vue
@@ -1,14 +1,24 @@
 <template>
 	<view class="context">
-		<u-tabbar :value="index" @change="change" :fixed="true" :placeholder="true" :safeAreaInsetBottom="true"
-			activeColor="#ffce56">
-			<!-- <u-tabbar-item text="首页" icon="home" @click="click(0)"></u-tabbar-item> -->
+		<!-- <u-tabbar :value="index" @change="ChangBar" :fixed="true" :placeholder="true" :safeAreaInsetBottom="true"
+			activeColor="#ffce56"> -->
+		<!-- <u-tabbar-item text="首页" icon="home" @click="click(0)"></u-tabbar-item> -->
 
-			<u-tabbar-item text="首页">
-				<image class="slot-icon" slot="active-icon" src="@/static/icons/home.png">
+		<!-- <u-tabbar-item text="首页" @click="click(0)">
+				<image class="slot-icon" slot="active-icon" src="@/static/icons/tabBar/home.png">
+				</image>
+				<image class="slot-icon" slot="inactive-icon" src="@/static/icons/tabBar/home-selected.png">
 				</image>
 			</u-tabbar-item>
 
+			<u-tabbar-item text="校园墙" icon="moments" @click="click(1)"></u-tabbar-item>
+			<u-tabbar-item text="消息" icon="chat" @click="click(2)"></u-tabbar-item>
+			<u-tabbar-item text="我的" icon="account" @click="click(3)"></u-tabbar-item>
+		</u-tabbar> -->
+
+		<u-tabbar :value="index" @change="name => index = name" :fixed="true" :placeholder="true"
+			:safeAreaInsetBottom="true">
+			<u-tabbar-item text="首页" icon="home" @click="click(0)"></u-tabbar-item>
 			<u-tabbar-item text="校园墙" icon="moments" @click="click(1)"></u-tabbar-item>
 			<u-tabbar-item text="消息" icon="chat" @click="click(2)"></u-tabbar-item>
 			<u-tabbar-item text="我的" icon="account" @click="click(3)"></u-tabbar-item>
@@ -25,36 +35,26 @@
 			}
 		},
 		methods: {
-			change(name) {},
-
 			click(index) {
-				if (this.index == index) {
-					uni.startPullDownRefresh()
-					// 2秒后强制停止刷新
-					setTimeout(function() {
-						uni.stopPullDownRefresh()
-					}, 2000)
-				} else {
-					let pageUrl = 'pages/home/index'
-					switch (index) {
-						case 0:
-							pageUrl = "/pages/home/index"
-							break
-						case 1:
-							pageUrl = "/pages/forum/index"
-							break
-						case 2:
-							pageUrl = "/pages/message/index"
-							break
-						case 3:
-							pageUrl = "/pages/mine/index"
-							break
-					}
-					uni.switchTab({
-						url: pageUrl
-					});
-					this.index = index
+				let pageUrl = 'pages/home/index'
+				switch (index) {
+					case 0:
+						pageUrl = "/pages/home/index"
+						break
+					case 1:
+						pageUrl = "/pages/forum/index"
+						break
+					case 2:
+						pageUrl = "/pages/message/index"
+						break
+					case 3:
+						pageUrl = "/pages/mine/index"
+						break
 				}
+				uni.switchTab({
+					url: pageUrl
+				});
+				this.index = index
 			}
 		}
 	}
