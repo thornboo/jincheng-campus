871bd83027bbe42a4fc18177f1751db2bd0359b6
thornboo <zhaozhuang1210@gmail.com>
2022-10-07 21:30:05 +0800

    :bug: fix: vAT code
    But there is no way to switch pages through Tabbar

--- components/TabBar/index.vue
+++ components/TabBar/index.vue
@@ -1,25 +1,7 @@
 <template>
 	<view class="context">
-		<!-- <u-tabbar :value="index" @change="ChangBar" :fixed="true" :placeholder="true" :safeAreaInsetBottom="true"
-			activeColor="#ffce56"> -->
-		<!-- <u-tabbar-item text="首页" icon="home" @click="click(0)"></u-tabbar-item> -->
-
-		<!-- <u-tabbar-item text="首页" @click="click(0)">
-				<image class="slot-icon" slot="active-icon" src="@/static/icons/tabBar/home.png">
-				</image>
-				<image class="slot-icon" slot="inactive-icon" src="@/static/icons/tabBar/home-selected.png">
-				</image>
-			</u-tabbar-item>
-
-			<u-tabbar-item text="校园墙" icon="moments" @click="click(1)"></u-tabbar-item>
-			<u-tabbar-item text="消息" icon="chat" @click="click(2)"></u-tabbar-item>
-			<u-tabbar-item text="我的" icon="account" @click="click(3)"></u-tabbar-item>
-		</u-tabbar> -->
-
 		<u-tabbar :value="index" @change="name => index = name" :fixed="true" :placeholder="true" :border="true"
 			:safeAreaInsetBottom="true" activeColor="#ffce56">
-
-			<!-- <u-tabbar-item text="首页" icon="home" @click="click(0)"></u-tabbar-item> -->
 
 			<u-tabbar-item text="首页" @click="click(0)">
 				<image class="u-page__item__slot-icon" slot="active-icon"
