48a25ca0b9507f1d53f6f94fd30ede6bc557ff21
thornboo <zhaozhuang1210@gmail.com>
2024-10-16 16:43:08 +0800

    feat: 新增发布悬浮按钮
    可自定义的悬浮按钮

---
+++ src/components/release-btn/release-btn.vue
@@ -0,0 +1,72 @@
+<template>
+	<view>
+		<wd-fab :direction="direction" :disabled="disabled" :position="position" :type="type">
+			<wd-button
+				v-for="(item, index) in titleList"
+				:key="index"
+				:disabled="disabled"
+				custom-class="custom-button"
+				round
+				type="primary"
+				@click="handleClick(item)"
+			>
+				<wd-icon name="star" size="40rpx"></wd-icon>
+			</wd-button>
+
+			<!--<wd-button-->
+			<!--	:disabled="disabled"-->
+			<!--	custom-class="custom-button"-->
+			<!--	round-->
+			<!--	type="primary"-->
+			<!--	@click="handleClick()"-->
+			<!--&gt;-->
+			<!--	<wd-icon name="star" size="40rpx"></wd-icon>-->
+			<!--</wd-button>-->
+			<!--<wd-button-->
+			<!--	:disabled="disabled"-->
+			<!--	custom-class="custom-button"-->
+			<!--	round-->
+			<!--	type="primary"-->
+			<!--	@click="handleClick('拾物发布')"-->
+			<!--&gt;-->
+			<!--	<wd-icon name="star" size="40rpx"></wd-icon>-->
+			<!--</wd-button>-->
+		</wd-fab>
+	</view>
+</template>
+
+<script lang="ts" setup>
+	const type = ref<'primary' | 'success' | 'info' | 'warning' | 'error' | 'default'>('primary')
+	const position = ref<'left-top' | 'right-top' | 'left-bottom' | 'right-bottom'>('right-bottom')
+	const direction = ref<'top' | 'right' | 'bottom' | 'left'>('top')
+	const disabled = ref<boolean>(false)
+	const titleList = ref(['11', '22'])
+
+	const handleClick = (param: string) => {
+		uni.navigateTo({
+			url: `/pages-sub/lost-found/lost-form?title=${param}`,
+			success: () => {
+				console.log('success')
+			},
+			fail: () => {
+				console.log('success')
+			},
+		})
+	}
+</script>
+
+<style lang="scss" scoped>
+	:deep(.custom-button) {
+		border-radius: 16px !important;
+		box-sizing: border-box;
+		height: 32px !important;
+		margin: 8rpx;
+		min-width: auto !important;
+		width: 32px !important;
+	}
+
+	:deep(.custom-radio) {
+		height: 32px !important;
+		line-height: 32px !important;
+	}
+</style>
--- src/pages-sub/lost-found/lost-found.vue
+++ src/pages-sub/lost-found/lost-found.vue
@@ -6,8 +6,13 @@
 
 <template>
 	<view>我是失物招领</view>
+	<ReleaseBtn />
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+	import ReleaseBtn from '@/components/release-btn/release-btn.vue'
+
+	const titleList = ref(['失物登记', '招领登记'])
+</script>
 
 <style lang="scss" scoped></style>
